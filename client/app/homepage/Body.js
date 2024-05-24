e777744a933c0aa98e16c1843374549a3ebc2108
ruzell <arturocrisanto1@gmail.com>
2024-05-24 08:28:55 +0800

    adjustment

--- client/app/homepage/Body.js
+++ client/app/homepage/Body.js
@@ -3,22 +3,20 @@ export default function Body() {
   return (
     <div className="flex flex-row">
       <div className="">
-        <div className="bg-gray-100 w-[600px] h-[300px] m-2 py-4 flex flex-col">
-          <section className=" bg-slate-500">
-            <h2 className="border-b pb-2 text-3xl font-semibold tracking-wider">
-              MARK
-            </h2>
-          </section>
-          <section className=" bg-slate-600">
+        <div className="w-[600px] h-[300px] m-2 py-4 flex flex-col justify-center items-center">
+          <div className="transform scale-75 rotate-6 -translate-x-40 translate-y-15 ">
+            <h2 className="pb-2 text-3xl font-semibold tracking-wider">MARK</h2>
+          </div>
+          <div className="">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl italic">
               RUZELL
             </h1>
-          </section>
-          <section className=" bg-slate-700">
+          </div>
+          <div className="">
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
               MARAY
             </h3>
-          </section>
+          </div>
         </div>
       </div>
       <div>other side</div>
